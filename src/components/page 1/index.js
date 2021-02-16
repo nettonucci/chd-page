717f1eb7bc0ed0896bfcb1c6921576102db7001f
nettonucci <nettonucci@gmail.com>
2021-02-16 14:59:51 -0300

    Page 1

--- src/components/page 1/index.js
+++ src/components/page 1/index.js
@@ -1,39 +1,52 @@
 import React from 'react';
 import { UnderLineBlue } from '../../styled-components/page1';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 import headerimg from '../../assets/header-img.png';
 
-export default function page1() {
+const useStyles = makeStyles(theme => ({
+	root: {
+		flexGrow: 1,
+	},
+	paper: {
+		padding: theme.spacing(2),
+		textAlign: 'center',
+		color: theme.palette.text.secondary,
+	},
+}));
+
+export default function Page1() {
 	const preventDefault = event => event.preventDefault();
+	const [spacing, setSpacing] = React.useState(2);
+	const classes = useStyles();
+
+	const handleChange = event => {
+		setSpacing(Number(event.target.value));
+	};
 
 	return (
-		<div>
-			<div>
-				<p>Ar condicionado</p>
-				<p>do jeito certo</p>
-				<p>
-					A CHD foca na engenharia para trazer a melhor solução em
-					ar-condicionado do mercado
-				</p>
-				<p>Peça uma proposta</p>
-			</div>
-			<div>
-				<img
-					alt="header-img"
-					style={{
-						alignSelf: 'flex-end',
-					}}
-					src={headerimg}
-				/>
-			</div>
-			<div
-				style={{
-					position: 'absolute',
-					backgroundColor: '#0b81bb',
-					width: '100%',
-					height: 20,
-					bottom: 220,
-				}}
-			/>
+		<div className={classes.root}>
+			<Grid container spacing={3}>
+				<Grid item xs={6}>
+					<p>Ar condicionado</p>
+					<p>do jeito certo</p>
+					<p>
+						A CHD foca na engenharia para trazer a melhor solução em
+						ar-condicionado do mercado
+					</p>
+					<p>Peça uma proposta</p>
+				</Grid>
+				<Grid item xs={6}>
+					<img
+						alt="header-img"
+						style={{
+							alignSelf: 'flex-end',
+						}}
+						src={headerimg}
+					/>
+				</Grid>
+			</Grid>
 		</div>
 	);
 }
